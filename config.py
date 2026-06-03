f082d4caddb1c0f738281906f2ec55841c280e1b
Buddy <203337004+iambuddy01@users.noreply.github.com>
2026-06-03 19:02:59 +0530

    ᴅᴇᴠɴʙᴏᴛs

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    API_ID = 
-    API_HASH = ""
-    TOKEN = ""  
-    START_PIC = "" 
-    CHAT = ""    
+    API_ID = 36761373
+    API_HASH = "ad664cf3843f8d0eb319dd76b8218179"
+    TOKEN = "8663511437:AAGJpZewARgErKrWaOIjwVamUY3eIqL2zi4"  
+    START_PIC = "https://files.catbox.moe/4ansts.jpg"
+    CHAT = "https://t.me/devsbuilt"    
